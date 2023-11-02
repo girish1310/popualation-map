7fb49068bd687bda0f91c3f9cc7ceb98ee166796
ylcharan <ylcharan99@gmail.com>
2023-11-02 14:01:28 +0530

    added prisma

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,25 +1,30 @@
-// Get - getting data
-// POST - creating data
-// PUT - updating data
-
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
 export function GET(request: NextRequest) {
-  // fetch data from db
   return NextResponse.json([
-    { id: 1, name: "mosh" },
-    { id: 2, name: "charan" },
+    {
+      id: 1,
+      price: 1.75,
+      name: "Milk",
+    },
+    {
+      id: 2,
+      price: 4.5,
+      name: "Iphone",
+    },
   ]);
 }
-
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(
       { error: validation.error.errors },
-      { status: 400 }
+      { status: 404 }
     );
-  return NextResponse.json({ id: 1, name: body.name });
+  return NextResponse.json(
+    { id: 1, name: body.name, price: body.price },
+    { status: 201 }
+  );
 }
--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 export function GET(request: NextRequest, params: { id: number }) {
   // fetch data from db
@@ -10,11 +11,21 @@ export function GET(request: NextRequest, params: { id: number }) {
 export async function PUT(request: NextRequest, params: { id: number }) {
   const body = await request.json();
   // bad request - 400
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
   // not found - 404
   if (params.id > 10)
     return NextResponse.json({ error: "user not found!!" }, { status: 404 });
 
   return NextResponse.json({ id: 1, name: body.name });
+}
+export async function DELETE(request: NextRequest, params: { id: number }) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User Not found" }, { status: 404 });
+
+  return NextResponse.json({});
 }
