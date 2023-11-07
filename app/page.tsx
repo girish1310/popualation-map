4ddb607f96fb03fee0ddc34d4fe0d0a84e273ec3
ylcharan <ylcharan99@gmail.com>
2023-11-07 21:47:20 +0530

    project updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
-import Image from "next/image";
 import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-[80px]">Home</h1>
-      <ProductCard />
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic rerum ab
-        error porro distinctio vel similique totam natus, consequatur veritatis
-        et, eius quod numquam magnam ipsum ducimus consequuntur eveniet. A.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui architecto
+        ratione aspernatur maiores provident, iusto error adipisci vel omnis
+        fugit facere cum ut ex voluptates. Impedit voluptatem, consequatur
+        expedita commodi ipsum eaque accusantium. Dolorem accusamus vitae
+        nesciunt, rerum ipsum libero architecto voluptatem neque consequatur
+        impedit commodi harum sequi! Reprehenderit aperiam ipsa eos labore
+        possimus. Excepturi fugiat est tempora fugit odio recusandae. Nisi
+        asperiores voluptates ab nobis minima. Odio temporibus aspernatur
+        officiis accusantium perspiciatis totam minus neque eos veniam a
+        sapiente non nostrum optio, dicta dolorem accusamus alias hic delectus
+        quod! Deleniti eligendi expedita voluptatum rem facere soluta laboriosam
+        necessitatibus facilis.
       </p>
-      <p>have to learn the next js asap</p>
+      <ProductCard />
     </>
   );
 }
